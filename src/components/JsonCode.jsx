e551c36bc4bbfc9a436e6c67034bf54c09ad30ba
wavec <wavecatch2020s@gmail.com>
2022-02-05 13:45:41 +0100

    second version

--- src/components/JsonCode.jsx
+++ src/components/JsonCode.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useRef } from "react";
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { dataActions } from "../store/data-slice";
 
 const JsonCode = () => {
   const textRef = useRef();
   const [jsonEdited, setJsonEdited] = useState(false);
+  const [showNotification, setShowNotification] = useState(false);
   const storeJsonData = useSelector((state) => state.data.data);
   const dispatch = useDispatch();
 
@@ -13,8 +15,12 @@ const JsonCode = () => {
   function submitJsonChanges() {
     const parsedJson = JSON.parse(textRef.current.value);
     setJsonEdited(false);
+    setShowNotification(true);
     dispatch(dataActions.loadJsonData(parsedJson));
   }
+  useEffect(() => {
+    setTimeout(setShowNotification, 5000);
+  }, [showNotification]);
 
   let btnStyle = "";
   if (jsonEdited) {
@@ -22,7 +28,6 @@ const JsonCode = () => {
   } else {
     btnStyle = {
       backgroundColor: "#cdd6df8d",
-      cursor: "not-allowed",
       color: "#33333350",
       boxShadow: "none",
     };
@@ -30,6 +35,11 @@ const JsonCode = () => {
 
   return (
     <div className="json-code-wrapper">
+      {showNotification ? (
+        <p className="notification">JSON Changed Successfully!</p>
+      ) : (
+        ""
+      )}
       <button onClick={submitJsonChanges} style={btnStyle}>
         Save JSON
       </button>
